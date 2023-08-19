fc41171edbe226bf5728c554d08b9f9fc23713a4
SajaRa20 <sajarabie2@gmail.com>
2023-08-19 20:21:31 +0300

    login

--- src/Components/Route/PublicRoute.js
+++ src/Components/Route/PublicRoute.js
@@ -1,25 +1,20 @@
+
 import React, { useContext } from 'react';
-import {  Route } from 'react-router-dom';
+import {  Outlet,Navigate } from 'react-router-dom';
 
 import { HOME_PAGE } from '../../Utils/routes.constant';
 
 import AuthContext from '../Context/AuthContext';
 
-function PublicRoute({ component: Component, ...rest }) {
+function PublicRoute() {
   const { isAuth } = useContext(AuthContext);
 
   return (
-    <Route {...rest} element={
-      !isAuth ? (
-        <Component />
-      ) : (
-        <Navigate to={HOME_PAGE} />
-      )
-    } />
+   
+      !isAuth ? <Outlet/> : <Navigate  to={HOME_PAGE} />
+
   );
 }
 
 
 export default PublicRoute;
-
-
